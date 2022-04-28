a201d8b9911c695e7e1f73fff8fe34ba18d5341c
luoboQAQ <qqhistory@126.com>
2022-04-28 16:43:52 +0800

    🌈 style(test): 将testbranch格式化

--- src/mips_test.v
+++ src/mips_test.v
@@ -2,17 +2,19 @@
 module mips_test();
 reg clk, rst;
 mips U_MIPS(
-         .clk(clk), .reset(rst)
+         .clk(clk),
+         .reset(rst)
      );
+
 initial begin
-    // $monitor("PC = 0x%8X, IR =0x%8X ,ALUOp = 0x%8X , HI = 0x%8X , LO =0x%8X", U_MIPS.U_PC.PC,U_MIPS.instr ,U_MIPS.U_ALU.ALUOp,U_MIPS.U_DIV.result1, U_MIPS.U_DIV.result2 );
-    clk = 1 ;
-    rst = 0 ;
-    #5 ;
-    rst = 1 ;
-    #20 ;
-    rst = 0 ;
+    clk = 1;
+    rst = 0;
+    #5;
+    rst = 1;
+    #20;
+    rst = 0;
 end
+
 always
     #(50) clk = ~clk;
 endmodule
